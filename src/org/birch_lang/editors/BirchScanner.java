d855be524f890a8df4831f0937df39cbbf3321b3
Lawrence Murray <lawrence.murray@uber.com>
2019-04-14 21:45:58 -0700

    Added "final" keyword, fixed some compile warnings, updated project settings.

--- src/org/birch_lang/editors/BirchScanner.java
+++ src/org/birch_lang/editors/BirchScanner.java
@@ -100,6 +100,7 @@ public class BirchScanner extends RuleBasedScanner {
 		keywordRule.addWord("super", keyword);
 		keywordRule.addWord("this", keyword);
 		keywordRule.addWord("parallel", keyword);
+		keywordRule.addWord("final", keyword);
 
 		literalRule.addWord("nil", literal);
 		literalRule.addWord("true", literal);
